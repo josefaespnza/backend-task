9d71121d27087b390d7536fda357bfe41139d64f
Josefa Espinoza <josefaespinoza@MacBook-Pro-de-Josefa.local>
2023-09-04 02:18:10 -0300

    feat: save csv in database.

--- src/routes/harvest.ts
+++ src/routes/harvest.ts
@@ -1,11 +1,17 @@
 import express from 'express';
+import multer from 'multer';
 import * as harvestServices from '../services/harvestService';
+import {parseFile} from '../services/csvServices';
+import { HarvestIn, NewHarvestInformation } from '../models/harvest/harvestInputModel';
+
+
 const router = express.Router();
+const upload = multer({dest: 'tmp/csv'});
 
 router.get('/', async (_req, res) =>{
     try{
         const harvests = await harvestServices.getAllHarvest();
-        res.send(harvests);
+        res.json(harvests);
     } catch(err: any){
         res.status(400).json({"error": err.message})
     }
@@ -13,31 +19,21 @@ router.get('/', async (_req, res) =>{
 });
 
 router.post('/', async (req, res) =>{
-    const {
-        farmerEmail,
-        farmerName,
-        farmerLastName,
-        clientEmail,
-        clientName,
-        clientLastName,
-        fieldName,
-        fieldLocation,
-        fruit,
-        harvestVariety
-    } = req.body;
+    const newHarvest = new NewHarvestInformation(
+        req.body.farmerEmail,
+        req.body.farmerName,
+        req.body.farmerLastName,
+        req.body.clientEmail,
+        req.body.clientName,
+        req.body.clientLastName,
+        req.body.fieldName,
+        req.body.fieldLocation,
+        req.body.fruit,
+        req.body.harvestVariety
+    )
+
     try {
-        await harvestServices.insertHarvest(
-            farmerEmail,
-            farmerName,
-            farmerLastName,
-            clientEmail,
-            clientName,
-            clientLastName,
-            fieldName,
-            fieldLocation,
-            fruit,
-            harvestVariety
-        )
+        await harvestServices.insertHarvest(newHarvest)
         res.send("the new haverst information was sucessfully created!");
     } catch (error: any){
         res.status(400).json({"error": error.message})
@@ -45,6 +41,15 @@ router.post('/', async (req, res) =>{
     
 });
 
-
+router.post('/csv', upload.single('file'),async (req, res) => {
+    const fileContents: Array<HarvestIn> = await parseFile((req as any).file.path);
+    try {
+        await harvestServices.insertMany(fileContents);
+        res.send("the data from the file was sucessfully saved!")
+    }catch (error: any){
+        res.status(400).json({"error": error.message})
+    }
+    
+})
 
 export default router;
--- src/services/harvestService.ts
+++ src/services/harvestService.ts
@@ -1,16 +1,34 @@
 import { getQuery, insertQuery } from '../database/helpers';
 import {open, close} from '../database/connection'
+import { HarvestIn } from '../models/harvest/harvestInputModel';
+import { HarvestInformation, HarvestOut } from '../models/harvest/harvestOutModel';
 
 const databasePath: string = "src/database/harvest.db";
 
 export async function getAllHarvest () {
     return new Promise((resolve, _reject) => {
+        const harvests: Array<HarvestOut> = [];
         const db = open(databasePath);
         db.all(getQuery, [], (err: any, rows: Array<any>) =>{
             if(err){
                 throw new Error(`Error ${err.message}`);
             }
-            resolve(rows);
+            rows.forEach(row => {
+                harvests.push(new HarvestInformation(
+                    row.harvest_id,
+                    row.farmer_email,
+                    row.farmer_name,
+                    row.farmer_last_name,
+                    row.client_email,
+                    row.client_name,
+                    row.client_last_name,
+                    row.field_name,
+                    row.field_location,
+                    row.fruit,
+                    row.harvest_variety
+                ))
+            });
+            resolve(harvests);
 
         });
         close(db);
@@ -21,30 +39,19 @@ export async function getAllHarvest () {
 
 }
 
-export async function insertHarvest(
-    farmerEmail: string,
-    farmerName: string,
-    farmerLastName: string,
-    clientEmail: string,
-    clientName: string,
-    clientLastName: string,
-    fieldName: string,
-    fieldLocation: string,
-    fruit: string,
-    harvestVariety: string,
-) {
+export async function insertHarvest(newHarvest: HarvestIn) {
     const db = open(databasePath);
     db.run(insertQuery, [
-        farmerEmail,
-        farmerName,
-        farmerLastName,
-        clientEmail,
-        clientName,
-        clientLastName,
-        fieldName,
-        fieldLocation,
-        fruit,
-        harvestVariety
+        newHarvest.farmerEmail,
+        newHarvest.farmerName,
+        newHarvest.farmerLastName,
+        newHarvest.clientEmail,
+        newHarvest.clientName,
+        newHarvest.clientLastName,
+        newHarvest.fieldName,
+        newHarvest.fieldLocation,
+        newHarvest.fruit,
+        newHarvest.harvestVariety
     ], (err: any)=>{
         if(err){
             throw new Error(`Error ${err.message}`);
@@ -54,4 +61,29 @@ export async function insertHarvest(
     
     close(db);
 
+}
+
+export async function insertMany(harvestArray: Array<HarvestIn>) {
+    const db = open(databasePath);
+    harvestArray.forEach(harvest =>{
+        db.run(insertQuery, [
+            harvest.farmerEmail,
+            harvest.farmerName,
+            harvest.farmerLastName,
+            harvest.clientEmail,
+            harvest.clientName,
+            harvest.clientLastName,
+            harvest.fieldName,
+            harvest.fieldLocation,
+            harvest.fruit,
+            harvest.harvestVariety
+        ], (err: any)=>{
+            if(err){
+                throw new Error(`Error ${err.message}`);
+            }
+            
+        })
+
+    })
+    close(db);
 }
